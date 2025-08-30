3271cae8f2a91f01b6b74fcb1b99c8e678c48ccf
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2025-08-30 22:34:06 +0530

    todo code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TodoAppComponent } from "./component/todo-app-component/todo-app-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ TodoAppComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/component/todo-app-component/todo-app-component.ts
+++ src/app/component/todo-app-component/todo-app-component.ts
@@ -1,11 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-todo-app-component',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './todo-app-component.html',
   styleUrl: './todo-app-component.css'
 })
 export class TodoAppComponent {
+ todos: Todo[] = [];
+  newTask: string = '';
 
+  addTask() {
+    if (this.newTask.trim() === '') return;
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: this.newTask,
+      completed: false
+    };
+
+    this.todos.push(newTodo);
+    this.newTask = ''; 
+  }
+
+  toggleComplete(todo: Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  deleteTask(id: number) {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  }
 }
