707635ff16760a1d9730521aeff3e06577be4b0b
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2025-08-30 21:43:10 +0530

    created todo component

---
+++ src/app/component/todo-app-component/todo-app-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-app-component',
+  imports: [],
+  templateUrl: './todo-app-component.html',
+  styleUrl: './todo-app-component.css'
+})
+export class TodoAppComponent {
+
+}
