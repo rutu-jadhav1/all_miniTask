171cfa1b3e61b228040c3b53c4d533fbc1f19458
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2025-08-30 23:17:58 +0530

    counter code

--- src/app/component/counter-component/counter-component.ts
+++ src/app/component/counter-component/counter-component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './counter-component.html',
   styleUrl: './counter-component.css'
 })
 export class CounterComponent {
+  count: number = 0;
+  maxLimit: number = 20;
 
+  increment() {
+    if (this.count < this.maxLimit) {
+      this.count++;
+    }
+  }
+
+  decrement() {
+    if (this.count > 0) {
+      this.count--;
+    }
+  }
+
+  reset() {
+    this.count = 0;
+  }
+  reachedLimit(): boolean {
+    return this.count === this.maxLimit;
+  }
 }
