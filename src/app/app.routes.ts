c189e7c85f53c8100b53281131a55048688db484
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2025-09-01 11:28:08 +0530

    Student list component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { TodoAppComponent } from './component/todo-app-component/todo-app-component';
 import { CounterComponent } from './component/counter-component/counter-component';
 import { NavbarComponent } from './component/navbar-component/navbar-component';
+import { StudentListComponent } from './component/student-list-component/student-list-component';
 
 export const routes: Routes = [
 
@@ -22,6 +23,10 @@ export const routes: Routes = [
                         path: 'counter',
                         component: CounterComponent
                     }, 
+                    {
+                        path: 'studentList',
+                        component: StudentListComponent
+                    }
                 ]
     }
 ];
